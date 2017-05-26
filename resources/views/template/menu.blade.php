c956cb0a58f41c2be7ee25f5881aeeb9e8822b22
Denis Valdivia <devalfe@gmail.com>
2017-05-25 21:19:24 -0500

    menu

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -29,15 +29,15 @@
                             </ul>
                           </div>
                           <div class="col-md-4 col-sm-6 has-sep">
-                            <ul class="nav">
-                              <li><a href="shop_grid.html"><span>Digital camera</span></a></li>
-                              <li><a href="shop_grid.html"><span>High-speed</span></a></li>
-                              <li><a href="shop_grid.html"><span>Camera phone</span> </a></li>
-                              <li><a href="shop_grid.html"><span>Multiplane</span> </a></li>
-                              <li> <a href="shop_grid.html"><span>Pocket camera</span></a></li>
-                              <li><a href="shop_grid.html"><span>Video camera</span></a></li>
-                              <li><a href="shop_grid.html"><span>Zenith camera</span></a></li>
-                              <li><a href="shop_grid.html"><span>Single-lens reflex</span></a></li>
+                            <ul class="nav flex-column">
+                              <li><a class="nav-link" href="shop_grid.html"><span>Digital camera</span></a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>High-speed</span></a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Camera phone</span> </a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Multiplane</span> </a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Pocket camera</span></a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Video camera</span></a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Zenith camera</span></a></li>
+                              <li><a class="nav-link" href="shop_grid.html"><span>Single-lens reflex</span></a></li>
                               <li><a href="shop_grid.html"><span>Light-field</span></a></li>
                             </ul>
                           </div>
@@ -59,38 +59,38 @@
                         <div class="row">
                           <div class="col-md-4 col-sm-6">
                             <h3>Dell</h3>
-                            <ul class="nav">
-                              <li><a href="shop_grid.html">Dell Inspiron 3558</a></li>
-                              <li><a href="shop_grid.html">Dell Adapter </a></li>
-                              <li><a href="shop_grid.html">Optical USB Mouse</a></li>
-                              <li><a href="shop_grid.html">Laptop Battery</a></li>
+                            <ul class="nav flex-column">
+                              <li><a class="nav-link" href="shop_grid.html">Dell Inspiron 3558</a></li>
+                              <li><a class="nav-link" href="shop_grid.html">Dell Adapter </a></li>
+                              <li><a class="nav-link" href="shop_grid.html">Optical USB Mouse</a></li>
+                              <li><a class="nav-link" href="shop_grid.html">Laptop Battery</a></li>
                             </ul>
                             <br>
                             <h3>Microsoft</h3>
-                            <ul class="nav">
-                              <li> <a href="shop_grid.html">Lumia 550 4G</a> </li>
-                              <li> <a href="shop_grid.html">Surface Pro 4</a> </li>
-                              <li> <a href="shop_grid.html">HTC Desire 620G</a> </li>
-                              <li> <a href="shop_grid.html">DMG Flip Cover</a> </li>
-                              <li> <a href="shop_grid.html">Silicone Keyboard</a> </li>
+                            <ul class="nav flex-column">
+                              <li> <a class="nav-link" href="shop_grid.html">Lumia 550 4G</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Surface Pro 4</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">HTC Desire 620G</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">DMG Flip Cover</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Silicone Keyboard</a> </li>
                             </ul>
                           </div>
                           <div class="col-md-4 col-sm-6 has-sep">
                             <h3>Apple</h3>
-                            <ul class="nav">
-                              <li> <a href="shop_grid.html">Apple Macbook Pro</a> </li>
-                              <li> <a href="shop_grid.html">Newest Apple Macbook Pro</a> </li>
-                              <li> <a href="shop_grid.html">Retina Display Laptop</a> </li>
-                              <li> <a href="shop_grid.html">Silicone Keyboard</a> </li>
+                            <ul class="nav flex-column">
+                              <li> <a class="nav-link" href="shop_grid.html">Apple Macbook Pro</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Newest Apple Macbook Pro</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Retina Display Laptop</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Silicone Keyboard</a> </li>
                             </ul>
                             <br>
                             <h3>Lenovo</h3>
-                            <ul class="nav">
-                              <li> <a href="shop_grid.html">Lenovo Yoga 300</a> </li>
-                              <li> <a href="shop_grid.html">Lenovo IdeaPad</a> </li>
-                              <li> <a href="shop_grid.html">Tab 3 A710F Tablet</a> </li>
-                              <li> <a href="shop_grid.html">Channel Speakers</a> </li>
-                              <li> <a href="shop_grid.html">Accessories</a> </li>
+                            <ul class="nav flex-column">
+                              <li> <a class="nav-link" href="shop_grid.html">Lenovo Yoga 300</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Lenovo IdeaPad</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Tab 3 A710F Tablet</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Channel Speakers</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html">Accessories</a> </li>
                             </ul>
                           </div>
                           <div class="col-md-4 has-sep hidden-sm">
@@ -124,23 +124,23 @@
                         <div class="row">
                           <div class="col-sm-6">
                             <h3>iPhone</h3>
-                            <ul class="nav">
-                              <li> <a href="shop_grid.html"> iPhone SE </a> </li>
-                              <li> <a href="shop_grid.htmls"> iPhone 6s Plus </a> </li>
-                              <li> <a href="shop_grid.html"> iPhone 3G </a> </li>
-                              <li> <a href="shop_grid.html"> iPad Pro </a> </li>
+                            <ul class="nav flex-column">
+                              <li> <a class="nav-link" href="shop_grid.html"> iPhone SE </a> </li>
+                              <li> <a class="nav-link" href="shop_grid.htmls"> iPhone 6s Plus </a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html"> iPhone 3G </a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html"> iPad Pro </a> </li>
                             </ul>
                           </div>
                           <div class="col-sm-6 has-sep">
                             <h3> Watch </h3>
-                            <ul class="nav">
-                              <li> <a href="shop_grid.html"> Quartz Watches </a> </li>
-                              <li> <a href="shop_grid.html"> Lover's Watches</a> </li>
-                              <li> <a href="shop_grid.html"> Digital Watches </a> </li>
-                              <li> <a href="shop_grid.html"> Sport Watch </a> </li>
+                            <ul class="nav flex-column">
+                              <li> <a class="nav-link" href="shop_grid.html"> Quartz Watches </a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html"> Lover's Watches</a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html"> Digital Watches </a> </li>
+                              <li> <a class="nav-link" href="shop_grid.html"> Sport Watch </a> </li>
                             </ul>
                           </div>
-                          <div class="col-sm-12"> <a class="ads1" href="#"><img class="img-responsive" src="images/menu-img3.jpg" alt=""></a> </div>
+                          <div class="col-sm-12"> <a class="ads1" href="#"><img class="img-fluid" src="images/menu-img3.jpg" alt=""></a> </div>
                         </div>
                       </div>
                     </div>
@@ -152,17 +152,17 @@
                    <a class="nav-link" href="shop_grid.html"><i class="icon fa fa-headphones fa-fw"></i> Headphones</a>
                     <div class="wrap-popup column1">
                       <div class="popup">
-                        <ul class="nav">
-                          <li><a href="shop_grid.html"><span>Envent Stereo</span></a></li>
-                          <li><a href="shop_grid.html"><span>Sennheiser</span></a></li>
-                          <li><a href="shop_grid.html"><span>Philips</span></a></li>
-                          <li><a href="shop_grid.html"><span>Sony</span></a></li>
-                          <li><a href="shop_grid.html"><span>Avantree</span></a></li>
-                          <li><a href="shop_grid.html"><span>Bajaao</span></a></li>
-                          <li><a href="shop_grid.html"><span>FiiO</span></a></li>
-                          <li><a href="shop_grid.html"><span>Audio-Technica</span></a></li>
-                          <li><a href="shop_grid.html"><span>LUXA2</span></a></li>
-                          <li><a href="shop_grid.html"><span>Geekria</span></a></li>
+                        <ul class="nav flex-column">
+                          <li><a class="nav-link" href="shop_grid.html"><span>Envent Stereo</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Sennheiser</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Philips</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Sony</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Avantree</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Bajaao</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>FiiO</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Audio-Technica</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>LUXA2</span></a></li>
+                          <li><a class="nav-link" href="shop_grid.html"><span>Geekria</span></a></li>
                         </ul>
                       </div>
                     </div>
@@ -171,11 +171,11 @@
                   <a class="nav-link" href="#"><i class="icon fa fa-microphone fa-fw"></i> Accessories</a>
                     <div class="wrap-popup column1">
                       <div class="popup">
-                        <ul class="nav">
-                          <li> <a href="shop_grid.html"> Vacuum Cleaner </a> </li>
-                          <li> <a href="shop_grid.html"> Memore Bluetooth</a> </li>
-                          <li> <a href="shop_grid.html"> On-Ear Headphones </a> </li>
-                          <li> <a href="shop_grid.html"> Digital MP3 Player </a> </li>
+                        <ul class="nav flex-column">
+                          <li> <a class="nav-link" href="shop_grid.html"> Vacuum Cleaner </a> </li>
+                          <li> <a class="nav-link" href="shop_grid.html"> Memore Bluetooth</a> </li>
+                          <li> <a class="nav-link" href="shop_grid.html"> On-Ear Headphones </a> </li>
+                          <li> <a class="nav-link" href="shop_grid.html"> Digital MP3 Player </a> </li>
                         </ul>
                       </div>
                     </div>
